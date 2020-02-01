2f03e1e8c52c4ddecf763f0d9461fed9b5e7fd96
geekerstar <247507792@qq.com>
2020-02-01 11:21:20 +0800

    接口限流和AOP记录操作日志

--- src/main/java/com/geekerstar/common/aspect/ControllerEndpointAspect.java
+++ src/main/java/com/geekerstar/common/aspect/ControllerEndpointAspect.java
@@ -4,12 +4,17 @@ import com.geekerstar.common.annotation.ControllerEndPoint;
 import com.geekerstar.common.exception.GeekException;
 import com.geekerstar.common.util.GeekUtil;
 import com.geekerstar.common.util.HttpContextUtil;
+import com.geekerstar.monitor.service.ILogService;
 import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
@@ -23,12 +28,15 @@ import java.lang.reflect.Method;
 @Component
 public class ControllerEndpointAspect extends AspectSupport {
 
+    @Autowired
+    private ILogService logService;
+
     @Pointcut("@annotation(com.geekerstar.common.annotation.ControllerEndPoint)")
     public void pointcut() {
     }
 
     @Around("pointcut()")
-    public Object around(ProceedingJoinPoint point) {
+    public Object around(ProceedingJoinPoint point) throws GeekException {
         Object result;
         Method targetMethod = resolveMethod(point);
         ControllerEndPoint annotation = targetMethod.getAnnotation(ControllerEndPoint.class);
@@ -37,8 +45,13 @@ public class ControllerEndpointAspect extends AspectSupport {
         try {
             result = point.proceed();
             if (StringUtils.isNotBlank(operation)) {
-                HttpServletRequest request = HttpContextUtil.getHttpServletRequest();
-                // todo 记录日志
+                RequestAttributes attributes = RequestContextHolder.getRequestAttributes();
+                ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) attributes;
+                String ip = StringUtils.EMPTY;
+                if (servletRequestAttributes != null) {
+                    ip = servletRequestAttributes.getRequest().getRemoteAddr();
+                }
+                logService.saveLog(point, targetMethod, ip, operation, start);
             }
             return result;
         } catch (Throwable throwable) {
@@ -48,5 +61,4 @@ public class ControllerEndpointAspect extends AspectSupport {
             throw new GeekException(error);
         }
     }
-
 }
