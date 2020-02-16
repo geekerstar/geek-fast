2129094ca8f2d8ea47e0b66155db887581c0df8f
geekerstar <247507792@qq.com>
2020-02-16 19:41:45 +0800

    RabbitMQ

---
+++ src/main/java/com/geekerstar/middleware/controller/ViewController.java
@@ -0,0 +1,23 @@
+package com.geekerstar.middleware.controller;
+
+import com.geekerstar.common.entity.GeekConstant;
+import com.geekerstar.common.util.GeekUtil;
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author geekerstar
+ * @date 2020/2/9 21:31
+ * @description
+ */
+@Api(tags = "中间件管理")
+@Controller("middleware")
+@RequestMapping(GeekConstant.VIEW_PREFIX + "middleware")
+public class ViewController {
+    @GetMapping("rabbitmq")
+    public String rabbitmq() {
+        return GeekUtil.view("middleware/rabbitmq");
+    }
+}
