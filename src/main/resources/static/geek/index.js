afff6bbc96446477729368208fab96676a33ecbf
geekerstar <247507792@qq.com>
2020-03-18 10:19:28 +0800

    web fix

--- src/main/resources/static/geek/index.js
+++
@@ -1,8 +0,0 @@
-layui.extend({
-    geek: 'lay/modules/geek',
-    validate: 'lay/modules/validate'
-}).define(['geek', 'conf'], function (exports) {
-    layui.geek.initPage();
-    console.log("\n 🐤", "color: #fff; font-size: .84rem;background: #366ed8; padding:5px 0;", "font-size: .84rem;background: #fff; border: 2px solid #b0e0a8;border-left: none; padding:3px 0;", " font-size: .84rem;background: #fcf9ec; padding:5px 0;margin-left: 8px");
-    exports('index', {});
-});
